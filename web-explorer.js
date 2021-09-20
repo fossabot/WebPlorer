20234076fca357c06ab26017aa86e4240fb105de
YuanYap <83793501+YuanYap@users.noreply.github.com>
2021-09-20 17:09:58 +0800

    Update web-explorer.js

--- web-explorer.js
+++ web-explorer.js
@@ -4,13 +4,14 @@
 var chalk = require('chalk');
 
 
-
+//KYuan and KChuen 2021 GPL-3.0
 let path = require('path')
 let express = require('express')
 let contentDisposition = require('content-disposition')
 let pkg = require(path.join(__dirname, 'package.json'))
 let scan = require('./scanner')
 
+//KYuan and KChuen 2021 GPL-3.0
 let program = require('commander')
 
 program
@@ -20,13 +21,13 @@ program
 
 let port = program.port || 3000
 
-
+//KYuan and KChuen 2021 GPL-3.0
 let tree = scan('.', 'files')
 
 
 let app = express()
 
-
+//KYuan and KChuen 2021 GPL-3.0
 app.use('/', express.static(path.join(__dirname, 'frontend')))
 
 
@@ -37,7 +38,7 @@ app.use('/files', express.static(process.cwd(), {
     }
 }))
 
-
+//KYuan and KChuen 2021 GPL-3.0
 app.get('/scan', function(req, res) {
     res.send(tree)
 })
@@ -45,3 +46,5 @@ app.get('/scan', function(req, res) {
 app.listen(port)
 
 console.log(chalk.blue('web explorer is running on port ' + port))
+
+//KYuan and KChuen 2021 GPL-3.0
